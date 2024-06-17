fc627ebe70a06c8675fd92b4b178041fa90ae1df
Alexander McCord <alex@mccord.dev>
2024-06-16 18:53:26 -0700

    Clip comment.

--- src/Parsec.hs
+++ src/Parsec.hs
@@ -6,19 +6,6 @@ infixr 2 <|>
 
 infixr 3 <&>
 
--- I think we need a data structure that holds all the possible paths in a language
--- e.g. you can think of them in terms of sets, like union types -> disjunctive parselets.
---
--- Then we need to walk through each paths in the graph, keeping a stack of parselets
--- we've seen so far. We'll then know that we've formed a cycle, which implies recursive parse.
---
--- One can view these parselets as function calls too. The way you loop things in Haskell is by
--- writing a recursive function. The way you loop things in parsers is by calling the same parser.
--- Such an example is `block` -> `[statement, block]`.
---
--- We'll want to come up with a DSL to generate the state machine graph.
--- Then we can go ahead and walk that graph.
-
 data (Show a) => Parser a
   = Ok
   | Char Char
