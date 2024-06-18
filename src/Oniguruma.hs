96f45930c431d9f350899245399fa7c99b46464c
Alexander McCord <alex@mccord.dev>
2024-06-18 00:32:53 -0700

    Rename Concat to Juxta.

--- src/Oniguruma.hs
+++ src/Oniguruma.hs
@@ -11,7 +11,7 @@ import Parsec
 data OnigurumaIR
   = Token Text
   | CharClass (Set Char)
-  | Concat OnigurumaIR OnigurumaIR
+  | Juxta OnigurumaIR OnigurumaIR
   | Alter OnigurumaIR OnigurumaIR
   | Repeat OnigurumaIR
   | Except OnigurumaIR
@@ -23,14 +23,14 @@ emptyToken = Token Text.empty
 toOnigurumaIR :: Parser a -> OnigurumaIR
 toOnigurumaIR Ok = emptyToken
 toOnigurumaIR (Char c) = Token (Text.singleton c)
-toOnigurumaIR (And a b) = Concat (toOnigurumaIR a) (toOnigurumaIR b)
+toOnigurumaIR (And a b) = Juxta (toOnigurumaIR a) (toOnigurumaIR b)
 toOnigurumaIR (Or a b) = Alter (toOnigurumaIR a) (toOnigurumaIR b)
 toOnigurumaIR (Many a) = Repeat (toOnigurumaIR a)
 
 render :: OnigurumaIR -> Text
 render (Token t) = t
 render (CharClass s) = renderClass s "[" "]"
-render (Concat a b) = renderConcat a b
+render (Juxta a b) = renderJuxta a b
 render (Alter a b) = renderAlter a b
 render (Repeat a) = renderRepeat a
 render (Except a) = renderExcept a
@@ -43,8 +43,8 @@ renderClassList :: [Char] -> Text -> Text -> Text
 renderClassList [c] _ _ = Text.singleton c
 renderClassList s l r = l <> Text.pack s <> r
 
-renderConcat :: OnigurumaIR -> OnigurumaIR -> Text
-renderConcat a b = render a <> render b
+renderJuxta :: OnigurumaIR -> OnigurumaIR -> Text
+renderJuxta a b = render a <> render b
 
 renderAlter :: OnigurumaIR -> OnigurumaIR -> Text
 renderAlter a b = "(" <> render a <> "|" <> render b <> ")"
@@ -56,7 +56,7 @@ renderRepeat a = render a <> "*"
 renderExcept :: OnigurumaIR -> Text
 renderExcept (Token t) = "(?!" <> t <> ")"
 renderExcept (CharClass s) = renderClass s "[^" "]"
-renderExcept (Concat a b) = "(?!" <> renderConcat a b <> ")"
+renderExcept (Juxta a b) = "(?!" <> renderJuxta a b <> ")"
 renderExcept (Alter a b) = "(?!" <> renderAlter a b <> ")"
 renderExcept (Repeat a) = "(?!" <> renderRepeat a <> ")"
 renderExcept (Except a) = render a
@@ -65,7 +65,7 @@ renderExcept (Optional a) = render (Except a)
 renderOptional :: OnigurumaIR -> Text
 renderOptional (Token t) = "(?:" <> t <> ")"
 renderOptional (CharClass s) = renderClass s "[" "]"
-renderOptional (Concat a b) = "(?:" <> renderConcat a b <> ")"
+renderOptional (Juxta a b) = "(?:" <> renderJuxta a b <> ")"
 renderOptional (Alter a b) = "(?:" <> renderAlter a b <> ")"
 renderOptional (Repeat a) = "(?:" <> renderRepeat a <> ")"
 renderOptional (Except _) = render emptyToken
